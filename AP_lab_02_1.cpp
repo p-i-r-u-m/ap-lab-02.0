ac3d440feb9a215ca0155c2c4024337cd32d965e
p-i-r-u-m <igr.case@gmail.com>
2024-09-16 07:20:57 +0300

    improve comments in code

--- AP_lab_02_1.cpp
+++ AP_lab_02_1.cpp
@@ -8,21 +8,22 @@
 #include <cmath>
 
 
-
 using namespace std;
 
 int main()
 {
-    double a;
-    double z1;
-    double z2;
+    double a; // angle alpha 
+    double z1; // result of problem 1
+    double z2; // result of problem 2
 
     cout << "Type angle alpha in radians:" << endl;
     cout << "a = "; cin >> a;
 
+    // Calculation with using library <cmath>
     z1 = ( sin(a) + sin(5*a) - sin(3*a) ) / ( cos(a) - cos(3*a) + cos(5*a) );
     z2 = tan(3*a);
 
+    // Result output
     cout << endl;
     cout << endl;
     cout << "- - - Result - - -" << endl;
